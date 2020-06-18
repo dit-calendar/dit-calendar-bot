0976e4684a5e8a39690e43d0f3c070f5189a5752
Viktor Befort <v.befort@googlemail.com>
2020-06-18 17:58:22 +0200

    update lib versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,15 +26,15 @@ repositories {
 }
 
 dependencies {
-    val fuelVersion = "2.2.1"
+    val fuelVersion = "2.2.3"
     val kittinunfResultVersion = "3.0.0"
     val konfigVersion = "1.6.10.0"
     val kotlinxSerializationVersion = "0.20.0"
-    val ktBotVersion = "1.3.3"
+    val ktBotVersion = "1.3.4"
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
-    implementation("com.github.kittinunf.fuel:fuel-kotlinx-serialization:$fuelVersion")
+    compileOnly("com.github.kittinunf.fuel:fuel-kotlinx-serialization:$fuelVersion")
     implementation("com.github.kittinunf.fuel:fuel-coroutines:$fuelVersion")
     implementation("com.github.kittinunf.result:result:$kittinunfResultVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$kotlinxSerializationVersion")
